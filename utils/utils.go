438b6db2898c629da32c2f66703eafbf0fba3739
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-26 23:34:43 +0700

    add seedCategory in utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,27 @@
 package utils
+
+import (
+	"expense-tracker-with-go/models"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+func SeedCategories(db *gorm.DB) {
+	categories := []models.Category{
+		{Name: "Makan"},
+		{Name: "Transportasi"},
+		{Name: "Gaji"},
+		{Name: "Hiburan"},
+	}
+
+	for _, category := range categories {
+		var existingCategory models.Category
+		if err := db.Where("name = ?", category.Name).First(&existingCategory).Error; err != nil {
+			db.Create(&category)
+			fmt.Println("Seeded category:", category.Name)
+		} else {
+			fmt.Println("Category already exists:", existingCategory.Name)
+		}
+	}
+}
