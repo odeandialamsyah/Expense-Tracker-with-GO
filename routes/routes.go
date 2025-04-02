1c4fcf21f302ba9144a6717d73e9c8a0d3fa009c
odeandialamsyah <odeandialamsyah@gmail.com>
2025-04-02 10:45:01 +0700

    create route end point for transaction controller

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,30 @@
 package routes
+
+import (
+	"expense-tracker-with-go/controllers"
+	"expense-tracker-with-go/repository"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func TransactionRoutes(router *gin.Engine, db *gorm.DB) {
+	//initialize repo
+	transactionRepo := repository.NewTransactionRepository(db)
+	categoryRepo := repository.NewCategoryRepository(db)
+
+	//initialize controller with repo
+	tc := controllers.TransactionController {
+		TransactionRepo: *transactionRepo,
+		CategoryRepo: *categoryRepo,
+	}
+
+	//define routes
+	transactionGroup := router.Group("/transaction") 
+	{
+		transactionGroup.GET("", tc.GetTransactions)
+		transactionGroup.POST("", tc.CreateTransaction)
+		transactionGroup.PUT("/:id", tc.UpdateTransaction)
+		transactionGroup.DELETE("/:id", tc.DeleteTransaction)
+	}
+}
