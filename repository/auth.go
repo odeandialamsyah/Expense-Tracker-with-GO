e9f3d4b828aecd540a46e41ab83a1fe72e21ec59
odeandialamsyah <odeandialamsyah@gmail.com>
2025-04-04 10:25:28 +0700

    create func create and get in auth repo

--- repository/auth.go
+++ repository/auth.go
@@ -1,1 +1,27 @@
 package repository
+
+import (
+	"expense-tracker-with-go/models"
+
+	"gorm.io/gorm"
+)
+
+type userRepository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) userRepository {
+	return userRepository{DB: db}
+}
+
+func (ur *userRepository) CreateUser(user *models.User) error {
+	return ur.DB.Create(user).Error
+}
+
+func (ur *userRepository) GetUserByEmail(email string) (*models.User, error) {
+	var user models.User
+	if err := ur.DB.Preload("Role").Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
